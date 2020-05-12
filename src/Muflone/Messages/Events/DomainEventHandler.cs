d9b2f3e52a0e08320c90a5baf6e88b5353a28c97
Alessandro Colla <alessandro.colla@outlook.com>
2020-05-12 18:49:16 +0200

    Add missing interface IPersistor

--- src/Muflone/Messages/Events/DomainEventHandler.cs
+++ src/Muflone/Messages/Events/DomainEventHandler.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Muflone.Messages.Events;
+using Muflone.Persistence;
 
 namespace Muflone.Messages.Commands
 {
---
+++ src/Muflone/Persistence/IPersister.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Muflone.Persistence
+{
+  public interface IPersister
+  {
+    Task<T> GetBy<T>(string id) where T : class;
+    Task Insert<T>(T entity) where T : class;
+    Task Update<T>(T entity) where T : class;
+    Task Delete<T>(T entity) where T : class;
+  }
+}
