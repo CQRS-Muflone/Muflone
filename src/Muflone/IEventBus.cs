b3253a00dedee09d25da9e949315a0e803efa89e
Alberto Acerbis <alberto.acerbis@intre.it>
2022-06-03 22:03:01 +0200

    IEventBus
    - Add generics to PublishAsync

--- src/Muflone/IEventBus.cs
+++ src/Muflone/IEventBus.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading.Tasks;
 using Muflone.Messages;
+using Muflone.Messages.Events;
 
 namespace Muflone;
 
 public interface IEventBus
 {
-	Task PublishAsync(IMessage @event);
+	Task PublishAsync<T>(IMessage @event) where T : class, IDomainEvent;
 }
