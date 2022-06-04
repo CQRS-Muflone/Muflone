f900b2d211c4cd3b56675be7a4378ced894fc42d
Alessandro Colla <alessandro.colla@evoluzionetelematica.it>
2022-06-04 16:48:03 +0100

    Revert "IEventBus"
    This reverts commit b3253a00dedee09d25da9e949315a0e803efa89e.

--- src/Muflone/IEventBus.cs
+++ src/Muflone/IEventBus.cs
@@ -1,10 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Muflone.Messages;
-using Muflone.Messages.Events;
 
 namespace Muflone;
 
 public interface IEventBus
 {
-	Task PublishAsync<T>(IMessage @event) where T : class, IDomainEvent;
+	Task PublishAsync(IMessage @event);
 }
