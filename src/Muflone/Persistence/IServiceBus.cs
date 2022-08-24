55f2b27d24b5e7f1348e5b9acdae82d335a5300c
Alessandro Colla <alessandro.colla@evoluzionetelematica.it>
2022-08-24 19:14:37 +0200

    Add serializer base

---
+++ src/Muflone/Persistence/IServiceBus.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Muflone.Messages.Commands;
+
+namespace Muflone.Persistence;
+
+public interface IServiceBus
+{
+    Task SendAsync<T>(T command, CancellationToken cancellationToken = default) where T : class, ICommand;
+}
