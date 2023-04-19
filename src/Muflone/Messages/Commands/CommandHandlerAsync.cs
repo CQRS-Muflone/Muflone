908c37599f9918de649244b5a67be459876e4db6
Alessandro Colla <eglathon@gmail.com>
2023-04-19 14:56:55 +0200

    Add concrete Command and Event handlers

---
+++ src/Muflone/Messages/Commands/CommandHandlerAsync.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Muflone.Persistence;
+
+namespace Muflone.Messages.Commands;
+
+public abstract class CommandHandlerAsync<TCommand> : ICommandHandlerAsync<TCommand> where TCommand : class, ICommand
+{
+	protected readonly IRepository Repository;
+	protected readonly ILogger Logger;
+
+	protected CommandHandlerAsync(IRepository repository, ILoggerFactory loggerFactory)
+	{
+		Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+		Logger = loggerFactory.CreateLogger(GetType());
+	}
+
+	public abstract Task HandleAsync(TCommand command, CancellationToken cancellationToken = new());
+
+
+	#region Dispose
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+		}
+	}
+
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+
+	~CommandHandlerAsync()
+	{
+		Dispose(false);
+	}
+
+	#endregion
+}
