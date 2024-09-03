fbf31f1f3e36dabcfeac669ce0b71788ce3ba146
Alessandro Colla <eglathon@gmail.com>
2024-09-03 17:07:23 +0200

    move IConsumer in core

---
+++ src/Muflone/IConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Muflone.Messages.Commands;
+using Muflone.Messages.Events;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Muflone;
+
+public interface IConsumer
+{
+	Task StartAsync(CancellationToken cancellationToken = default);
+	Task StopAsync(CancellationToken cancellationToken = default);
+}
+
+public interface IDomainEventConsumer<in T> : IConsumer where T : DomainEvent
+{
+	Task ConsumeAsync(T message, CancellationToken cancellationToken = default);
+}
+
+public interface IIntegrationEventConsumer<in T> : IConsumer where T : IntegrationEvent
+{
+	Task ConsumeAsync(T message, CancellationToken cancellationToken = default);
+}
+
+public interface ICommandConsumer<in T> : IConsumer where T : Command
+{
+	Task ConsumeAsync(T message, CancellationToken cancellationToken = default);
+}
